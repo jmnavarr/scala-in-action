7117a151bcbfa6b828db690c07fea62c6a9114c5
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-03-25 18:53:36 +0100

    Implementation for automated resource management

--- src/main/scala/AutomatedResourceManagement.scala
+++ src/main/scala/AutomatedResourceManagement.scala
@@ -3,13 +3,19 @@ import scala.io.Source
 
 object AutomatedResourceManagement {
 
+  type Closable = {
+    def close(): Unit
+  }
+
+  def withResource[A <: Closable, B](resource: A)(f: A => B): B =
+    try f(resource) finally resource.close()
+
   def main(args: Array[String]): Unit = {
     val in = new FileInputStream("build.sbt")
     val lineCount =
-      try
-        Source.fromInputStream(in).getLines().size
-      finally
-        in.close() // Don't forget to close!
+      withResource(in) { resource =>
+        Source.fromInputStream(resource).getLines().size
+      }
     println(lineCount)
   }
 }
