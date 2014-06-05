332bcb34074e8b6f8bcd09d7ea738cdef555bce4
Heiko Seeberger <heiko.seeberger@gmail.com>
2014-06-05 16:55:15 +0200

    Set up project

--- project/Common.scala
+++ project/Common.scala
@@ -20,8 +20,6 @@ object Common {
         "-target:jvm-1.7",
         "-encoding", "UTF-8"
       ),
-      unmanagedSourceDirectories in Compile := List((scalaSource in Compile).value),
-      unmanagedSourceDirectories in Test := List((scalaSource in Test).value),
       // Scalariform settings
       ScalariformKeys.preferences := ScalariformKeys.preferences.value
         .setPreference(AlignSingleLineCaseStatements, true)
