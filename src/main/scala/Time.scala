be5790b75e59865025cd836de05b5f9fc97b8318
Heiko Seeberger <mail@heikoseeberger.de>
2015-10-10 16:41:12 +0200

    Basic OO – methods

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -1,1 +1,6 @@
-class Time(val hours: Int, val minutes: Int)
+class Time(val hours: Int, val minutes: Int) {
+
+  def minus(that: Time): Int = this.asMinutes - that.asMinutes
+
+  private def asMinutes = hours * 60 + minutes
+}
