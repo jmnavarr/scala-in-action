2e6e27adfb359e4afa72a678276e9577e7c1b8c8
Heiko Seeberger <mail@heikoseeberger.de>
2015-10-10 17:43:43 +0200

    Structural types and upper bounds – automated resource management

--- src/main/scala/AutomatedResourceManagement.scala
+++ src/main/scala/AutomatedResourceManagement.scala
@@ -3,9 +3,16 @@ import scala.io.Source
 
 object AutomatedResourceManagement {
 
+  type Closable = {
+    def close(): Unit
+  }
+
+  def withResource[A <: Closable, B](a: A)(f: A => B): B = try f(a) finally a.close()
+
   def main(args: Array[String]): Unit = {
-    val in = new FileInputStream("build.sbt")
-    val contents = Source.fromInputStream(in).getLines().mkString(f"%n")
+    val contents = withResource(new FileInputStream("build.sbt")) { in =>
+      Source.fromInputStream(in).getLines().mkString(f"%n")
+    }
     println(s"""|```
                 |$contents
                 |```""".stripMargin)
