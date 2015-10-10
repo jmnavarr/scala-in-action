293b6541f9bbc62c54114ac0e02e4dcd363fa082
Heiko Seeberger <mail@heikoseeberger.de>
2015-10-10 16:49:49 +0200

    Basic OO – overriding

--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -4,5 +4,7 @@ case class Time(hours: Int = 0, minutes: Int = 0) {
 
   def -(that: Time): Int = minus(that)
 
+  override def toString = f"$hours%02d:$minutes%02d"
+
   private def asMinutes = hours * 60 + minutes
 }
